efc8cb2d9f9175413a17ed6d77b81c8e486f715d
PavansaiKaviti <146151676+PavansaiKaviti@users.noreply.github.com>
2024-03-06 11:10:20 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer fixed-bottom bg-dark">
       <div className="container text-center ">
-        <p className="text-white" style={{ margin: '10px' }}>&copy; 2023 Global Gagette. All rights reserved.</p>
+        <p className="text-white" style={{ margin: '10px' }}>&copy; 2023 Global Gazette. All rights reserved.</p>
       </div>
     </footer>
   );
